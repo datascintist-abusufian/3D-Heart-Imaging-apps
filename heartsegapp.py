e9ae3163585e32ab70924fe8a385eeb8ce1ec192
Data Analyst <abusufian.tex.cu@gmail.com>
2024-07-14 03:45:14 +0100

    Update heartsegapp.py

--- heartsegapp.py
+++ heartsegapp.py
@@ -188,7 +188,8 @@ def main():
             st.write("GIF downloaded successfully.")
         except Exception as e:
             st.error(f"Error downloading gif: {e}")
-if os.path.exists(gif_path):
+            
+    if os.path.exists(gif_path):
     try:
         st.image(gif_path, width=500)
     except Exception as e:
