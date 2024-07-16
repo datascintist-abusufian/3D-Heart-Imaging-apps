922147395e47fba850f92056d707abbceca97c30
Data Analyst <abusufian.tex.cu@gmail.com>
2024-07-16 02:55:45 +0100

    Update heartsegapp.py

--- heartsegapp.py
+++ heartsegapp.py
@@ -141,7 +141,7 @@ def image_input(src, model):
                 except Exception as e:
                     st.error(f"Error during prediction: {e}")
     elif src == 'From sample Images':
-    selected_image = st.sidebar.slider("Select random image from test set.", 1, 50)
+         selected_image = st.sidebar.slider("Select random image from test set.", 1, 50)
     image_name = f"{selected_image}.jpg"
     image_url = f"https://raw.githubusercontent.com/datascintist-abusufian/3D-Heart-Imaging-apps/main/data/images/test/{image_name}"
     try:
